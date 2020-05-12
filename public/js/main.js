2fc2e0d129eea6ee96ef9e843dd95a33d51f33ee
ryanpetersonOF <ryan@openfin.co>
2020-05-12 21:07:13 +0100

    Broken system events

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,48 @@
-// uncomment line below to register offline cache service worker 
-// navigator.serviceWorker.register('../serviceworker.js');
+let app;
+let last;
+let count = 0;
 
-if (typeof fin !== 'undefined') {
-    init();
-} else {
-    document.querySelector('#of-version').innerText =
-        'The fin API is not available - you are probably running in a browser.';
+function startApp() {
+    fin.Application.start({url: 'https://google.com', uuid: Math.random().toString(), name: Math.random().toString()}).then(application => app = application);
 }
 
-//once the DOM has loaded and the OpenFin API is ready
-async function init() {
-    //get a reference to the current Application.
-    const app = await fin.Application.getCurrent();
-    const win = await fin.Window.getCurrent();
+function closeApp() {
+    app.close();
+}
 
-    const ofVersion = document.querySelector('#of-version');
-    ofVersion.innerText = await fin.System.getVersion();
+function cycle() {
+    if (count > 0) {
+        last.teardown();
+    }
 
-    //Only launch new windows from the main window.
-    if (win.identity.name === app.identity.uuid) {
-        //subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
-        //for this app we will  launch a child window the first the user clicks on the desktop.
-        app.once('run-requested', async () => {
-            await fin.Window.create({
-                name: 'childWindow',
-                url: location.href,
-                defaultWidth: 320,
-                defaultHeight: 320,
-                autoShow: true
-            });
-        });
+    last = new test(count);
+    count++;
+}
+
+setInterval(() => {
+    document.getElementById('startedListeners').textContent = fin.System.listenerCount('application-started');
+}, 250)
+
+
+class test {
+    constructor(signature) {
+        this.sig = signature;
+        this.startRef = this.startListenerFunc.bind(this);
+        this.closeRef = this.closedListenerFunc.bind(this);
+        fin.System.addListener('application-started', this.startRef).then(() => console.log(this.sig, 'added application-started'));
+        fin.System.addListener('application-closed', this.closeRef).then(() => console.log(this.sig, 'added application-closed'));
+    }
+
+    startListenerFunc() {
+        console.log(this.sig, 'app starting');
+    }
+
+    closedListenerFunc() {
+        console.log(this.sig, 'app closing')
+    }
+
+    teardown() {
+        fin.System.removeListener('application-started', this.startRef).then(() => console.log(this.sig, 'removed application-started'));
+        fin.System.removeListener('application-closed',  this.closeRef).then(() => console.log(this.sig, 'removed application-closed'));
     }
 }
