565b503a9e7155f205c317f2ab1a35bcbafdf45c
ryanpetersonOF <ryan@openfin.co>
2020-05-13 18:06:34 +0100

    system panel mock

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,9 @@ fin.desktop.main((args)=>{
     console.log(window.location);
 
     fin.Application.getCurrentSync().setTrayIcon('http://localhost:5555/favicon.ico');
+
+    // system panel mock
+    
+    // const baseUrl = window.location.href.replace(/\/(index.html)?$/, '');
+    // fin.Application.getCurrentSync().setTrayIcon(`${baseUrl}/favicon.ico`);
 })
